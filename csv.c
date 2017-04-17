7b667a1cb02714f6ba97300b9caefb2da1376640
Riliumph <riliumph@gmail.com>
2017-04-18 03:08:22 +0900

    正しくダブルポインタを使うように修正

--- csv.c
+++ csv.c
@@ -26,7 +26,7 @@ static int count_line( const char* filename )
   return line_count;
 }
 
-int read_csv( const char* filename, data_t* data, int* data_num )
+int read_csv( const char* filename, data_t** data, int* data_num )
 {
   *data_num = count_line( filename );
 
@@ -36,8 +36,8 @@ int read_csv( const char* filename, data_t* data, int* data_num )
     exit( -1 );
   }
 
-  data = (data_t *)malloc( *data_num * sizeof( data_t ) );
-  if( data == NULL ){
+  *data = (data_t *)malloc( *data_num * sizeof( data_t ) );
+  if( *data == NULL ){
     printf( "Cannot allocate memory\n" );
     exit( -1 );
   }
@@ -49,10 +49,11 @@ int read_csv( const char* filename, data_t* data, int* data_num )
       continue;
     }
     sscanf( line, "%[^,],%[^,],%d,%d",
-            &data[idx].type,
-            &data[idx].cost,
-            &data[idx].hint,
-            &data[idx].prio );
+            &( *data + idx )->type,
+            ( *data + idx )->cost,
+            &( *data + idx )->hint,
+            &( *data + idx )->prio
+    );
     ++idx;
   }
   return 0;
--- main.c
+++ main.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "data.h"
 #include "csv.h"
 
 int main( void )
 {
-  data_t* data_list = 0;
+  data_t* data_list = NULL;
   int data_num;
-  read_csv( "./data.csv", data_list, &data_num );
+  read_csv( "./data.csv", &data_list, &data_num );
 
   for( int i = 0; i < data_num; ++i ){
     printf( "%d: %c, %s, %d, %d\n",
             i,
-            ( data_list + i )->type,
-            ( data_list + i )->cost,
-            ( data_list + i )->hint,
-            ( data_list + i )->prio );
+            data_list[i].type,
+            data_list[i].cost,
+            data_list[i].hint,
+            data_list[i].prio );
   }
   free( data_list );
   return 0;
